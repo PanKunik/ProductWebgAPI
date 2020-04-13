c14baa1ea5b422785f0b480e9f1b6479d7074c5f
PanKunik <kunik12254@o2.pl>
2020-04-13 23:11:23 +0200

    Modified Variant Table

--- PSMData/dbo/Tables/Variant.sql
+++ PSMData/dbo/Tables/Variant.sql
@@ -4,5 +4,7 @@
 	[ProductId] INT NOT NULL,
 	[BasePrice] DECIMAL(10, 2) NOT NULL,
 	[Tax] DECIMAL (10,2) NOT NULL,
-	[InStock] INT NOT NULL
+	[InStock] INT NOT NULL,
+	[CreationDate] DATETIME2(7) NOT NULL DEFAULT getutcdate(),
+	[ModificationDate] DATETIME2(7) NOT NULL DEFAULT getutcdate()
 )
