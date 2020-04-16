79e8cd218a10b62a89bb0bbb5aa42f6cacbf61e9
PanKunik <kunik12254@o2.pl>
2020-04-16 11:53:14 +0200

    Added WebAPI clinet MVC project

---
+++ PSMDataManagerMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PSMDataManagerMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PSMDataManagerMVC/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using PSMDataManagerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSMDataManagerMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Mąka Szymanowska typ 450",
+                Description = "Dobra mąka! Jedz!",
+                CategoryId = 1,
+                BrandId = 1,
+                VariantsCount = 3
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Mąka Babuni typ 650",
+                Description = "Mąka bardzo, bardzo dobra do wypieków! Piecz!",
+                CategoryId = 2,
+                BrandId = 1,
+                VariantsCount = 12
+            },
+        };
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
